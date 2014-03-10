f5c0c7e773beaa492584a1ba716aff1090bcf8d6
GBora <gpbora@gmail.com>
2014-03-10 16:52:42 +0200

    Added dependency on AngularFire and created factory for dealing with firebase data. Fixes #1

--- js/base.js
+++ js/base.js
@@ -1,18 +1,42 @@
 console.log("base.js has been loaded1");
 
-angular.module("myApp", [])
+angular.module("myApp", ["firebase"])
+    .factory("ChatService", ["$firebase",
+        function($firebase) {
+            var ref = new Firebase("https://popping-fire-9812.firebaseio.com/chat");
+            return $firebase(ref);
+        }
+    ])
+    .controller("myCtrl", ["$scope", "ChatService",
+        function($scope, chatService) {
+            console.log("ctrl has been instantiated");
+            $scope.messages = chatService;
 
-.controller("myCtrl", function($scope) {
-    console.log("ctrl has been instantiated");
-    $scope.msg = "hello";
+            $scope.login = function() {
 
-    $scope.login = function() {
+                console.log("name " + $scope.userName + " " + "password " + $scope.userPass);
+                $scope.user = {
+                    "name": $scope.userName,
+                    "pass": $scope.userPass
+                };
 
-        console.log("name " + $scope.userName + " " + "password " + $scope.userPass);
-        $scope.user = {
-            "name": $scope.userName,
-            "pass": $scope.userPass
-        };
+                $scope.getMessages = function($http) {
 
-    };
-});
+                }
+
+                $scope.send = function() {
+
+                    var msg = {
+                        "sender": $scope.userName,
+                        "recipient": $scope.messRecipient,
+                        "body": $scope.messBody
+                    }
+
+                    $scope.messages.$add(msg);
+
+                }
+
+
+            };
+        }
+    ]);
