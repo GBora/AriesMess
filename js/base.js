d9645ad6d29d47071476fd4639bb8dca6c7874b0
GBora <gpbora@gmail.com>
2014-03-13 11:41:03 +0200

    Added 3 way binding which allows messages to be seen. Still need to style the page better

--- js/base.js
+++ js/base.js
@@ -1,42 +1,43 @@
 console.log("base.js has been loaded1");
 
 angular.module("myApp", ["firebase"])
-    .factory("ChatService", ["$firebase",
-        function($firebase) {
-            var ref = new Firebase("https://popping-fire-9812.firebaseio.com/chat");
-            return $firebase(ref);
-        }
-    ])
-    .controller("myCtrl", ["$scope", "ChatService",
-        function($scope, chatService) {
-            console.log("ctrl has been instantiated");
-            $scope.messages = chatService;
+        .factory("ChatService", ["$firebase",
+            function($firebase) {
+                var ref = new Firebase("https://popping-fire-9812.firebaseio.com/chat");
+                return $firebase(ref);
+            }
+        ])
+        .controller("myCtrl", ["$scope", "ChatService",
+            function($scope, chatService) {
+                console.log("ctrl has been instantiated");
+                $scope.messages = chatService;
+                $scope.messageList = chatService;
 
-            $scope.login = function() {
+                $scope.login = function() {
 
-                console.log("name " + $scope.userName + " " + "password " + $scope.userPass);
-                $scope.user = {
-                    "name": $scope.userName,
-                    "pass": $scope.userPass
-                };
+                    console.log("name " + $scope.userName + " " + "password " + $scope.userPass);
+                    $scope.user = {
+                        "name": $scope.userName,
+                        "pass": $scope.userPass
+                    };
 
-                $scope.getMessages = function($http) {
+                    $scope.getMessages = function($http) {
 
-                }
-
-                $scope.send = function() {
-
-                    var msg = {
-                        "sender": $scope.userName,
-                        "recipient": $scope.messRecipient,
-                        "body": $scope.messBody
                     }
 
-                    $scope.messages.$add(msg);
+                    $scope.send = function() {
 
-                }
+                        var msg = {
+                            "sender": $scope.userName,
+                            "recipient": $scope.messRecipient,
+                            "body": $scope.messBody
+                        }
+
+                        $scope.messages.$add(msg);
+
+                    }
 
 
-            };
-        }
-    ]);
+                };
+            }
+        ]);
